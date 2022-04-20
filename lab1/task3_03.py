d669336b4acc1346b5c6171c1634b90bc86b23b9
Mcat7 <evgenij-stud@yandex.ru>
2022-04-20 12:44:21 +0300

    new tasks ready

--- lab1/task3_03.py
+++ lab1/task3_03.py
@@ -1,4 +1,4 @@
-# 15	Удалить элементы, индексы которых кратны 3.
+# 3	Удалить элементы, индексы которых кратны 7.
 
 import random
 n = int(input('Укажите размер списка: '))
@@ -6,8 +6,8 @@ A = [random.randint(0, 99) for _ in range(n)]
 
 B = []
 print(f'A: {A}')
-for i in range(0, len(A)):
-    if i % 3 != 0 or i == 0:
+for i in range(len(A)):
+    if i % 7 != 0 or i == 0:
         B.append(A[i])
 print(f'B: {B}')
 
--- lab1/task3_09.py
+++ lab1/task3_09.py
@@ -1,9 +1,10 @@
-# Найти среднее арифметическое элементов списка.
+# 9 Найти среднее арифметическое элементов списка.
 
 import random
+n = int(input('Укажите размер списка: '))
+A = [random.randint(0, 99) for _ in range(n)]
 
-arr = [random.randint(0, 200) for _ in range(10)]
 sum = 0
-for i in range(1, len(arr)):
-    sum += arr[i]
-print(f'Среднее = {sum/len(arr)}')
+for i in range(1, len(A)):
+    sum += A[i]
+print(f'Среднее = {sum/len(A)}')
