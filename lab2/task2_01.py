dad354b9401ce25202bce5fb3565ab0164aa1ffa
Mcat7 <evgenij-stud@yandex.ru>
2022-06-03 16:29:38 +0300

    ...

--- lab2/task2_01.py
+++ lab2/task2_01.py
@@ -1,12 +1,20 @@
-import numpy as np
+# 1. В матрице найти номер строки, сумма чисел в которой максимальна.
 
-A = np.array([[1, 2, 3], [2, 3, 4], [0, 0, 0], [1, 1, 1]])
+import random
+import numpy as np
+def randList():
+    return [random.randint(0, 5) for _ in range(5)]
+A = np.array([randList(),
+              randList(),
+              randList(),
+              randList()])
 maxrow_index = 0
 maxSum = sum(A[0])
 for i in range(1, len(A)):
     #print(sum(A[i]))
     if sum(A[i]) > maxSum:
         maxrow_index = i
-print(maxrow_index)
+        maxSum = sum(A[i])
 
-# for i in range
+print(A)
+print(f"Строка с максимальной суммой чисел: {maxrow_index+1}, сумма: {maxSum}")
