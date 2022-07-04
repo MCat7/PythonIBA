0af477b7e9b6ef75290669936d6af98edc0c3d40
FantomasI <e.strelchuk1.21@gmail.com>
2022-07-04 12:08:27 +0300

    lab5

--- lab3/task2.py
+++ lab3/task2.py
@@ -1,7 +1,11 @@
 import datetime
 
+a = 0
+
 
 def tribonacci(n):
+    global a
+    a += 1
     if n in (1, 2):
         return 0
     elif n == 3:
@@ -9,8 +13,11 @@ def tribonacci(n):
     return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
 
 
-n = 20
+n = 5
 t1 = datetime.datetime.now()
+print(f"Для числа {n} :")
 print(tribonacci(n))
 t2 = datetime.datetime.now()
-print(str((t2 - t1).total_seconds()))
+print(str((t2 - t1).total_seconds())+" секунд")
+print(f"кол-во обращений: {a}")
+
